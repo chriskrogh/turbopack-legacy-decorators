543fc6424dfeb70f20b269930da2e0dfd172725a
Christopher Krogh <35310373+chriskrogh@users.noreply.github.com>
2025-07-02 14:57:53 +0000

    Add missing observer

--- src/app/Client.tsx
+++ src/app/Client.tsx
@@ -1,17 +1,29 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useState } from "react";
+import { observer } from "mobx-react";
 import { CounterStore } from "./store";
-export default function Client() {
-    const store = useRef(new CounterStore());
 
-    return (
-        <div>
-            Counter: {store.current.counter}
-            <br />
-            Counter more than 3: {store.current.counterMoreThan3 ? "Yes" : "No"}
-            <br />
-            <button onClick={() => store.current.incrementCounter()}>Increment</button>
-        </div>
-    )
+const Counter = observer(() => {
+  const [counter] = useState(() => new CounterStore());
+
+  return (
+    <div>
+      Counter: {counter.counter}
+      <br />
+      Counter more than 3: {counter.counterMoreThan3 ? "Yes" : "No"}
+      <br />
+      <button onClick={() => counter.incrementCounter()}>Increment</button>
+    </div>
+  );
+});
+
+export default function Client() {
+  const [ssr, setSsr] = useState(true);
+
+  useEffect(() => {
+    setSsr(false);
+  }, []);
+
+  return ssr ? null : <Counter />;
 }

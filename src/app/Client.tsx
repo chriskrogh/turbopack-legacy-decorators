3eba6001b301054944faa49fd8a2d91366fe21e2
Christopher Krogh <35310373+chriskrogh@users.noreply.github.com>
2025-07-02 14:44:42 +0000

    Upgrade to next@canary

--- src/app/Client.tsx
+++ src/app/Client.tsx
@@ -1,17 +1,30 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { CounterStore } from "./store";
-export default function Client() {
-    const store = useRef(new CounterStore());
 
-    return (
-        <div>
-            Counter: {store.current.counter}
-            <br />
-            Counter more than 3: {store.current.counterMoreThan3 ? "Yes" : "No"}
-            <br />
-            <button onClick={() => store.current.incrementCounter()}>Increment</button>
-        </div>
-    )
+const Counter = () => {
+  const store = useRef(new CounterStore());
+
+  return (
+    <div>
+      Counter: {store.current.counter}
+      <br />
+      Counter more than 3: {store.current.counterMoreThan3 ? "Yes" : "No"}
+      <br />
+      <button onClick={() => store.current.incrementCounter()}>
+        Increment
+      </button>
+    </div>
+  );
+};
+
+export default function Client() {
+  const [ssr, setSsr] = useState(true);
+
+  useEffect(() => {
+    setSsr(false);
+  }, []);
+
+  return ssr ? null : <Counter />;
 }
